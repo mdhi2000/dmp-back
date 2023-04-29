6bcbfa6efd36fec50e068ecac6f0794448e2fcdc
Mohammad Habibi <mdhi2000@gmail.com>
2023-04-29 16:33:05 +0330

    initialize

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/dmp'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
